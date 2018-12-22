c4ac050ba4fe5ffd5c450ee7d6f65c25d76015c6
jokertin92 <jokertin92@yahoo.co.in>
2018-12-22 21:18:57 +0530

    Changes for LoggerModule

---
+++ Web/Handler/LogHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.IO;
+
+namespace SupplierDocuments.Handler
+{
+    public class LoggerModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += Context_BeginRequest;
+        }
+
+        #region Begin Request Handler
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            var requestContext = HttpContext.Current.Request;
+
+            /* Log the request details */
+            var httpMethod = requestContext.HttpMethod;
+
+            //Check if the http method is supposed to be logged
+            var canLog = Enum.GetNames(typeof(HttpMethods)).Contains(httpMethod);
+
+            if (canLog)
+            {
+                var logContext = requestContext.FilePath;
+            }
+        }
+
+        #endregion
+
+        #region Dispose Method
+
+        public void Dispose()
+        {
+
+        }
+
+        #endregion
+    }        
+}
+
+#region Enum for HTTP Verbs
+
+public enum HttpMethods
+{
+    GET = 1,
+    PUT,
+    POST,
+    DELETE
+}
+
+#endregion
